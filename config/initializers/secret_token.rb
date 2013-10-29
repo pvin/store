aff2171eb316850862c5ac49dab2b2bdf24f79e0
pvin <azri@azri-System-Product-Name.(none)>
2013-10-29 11:50:03 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '98e648b7ad447acd06afc49e43ceb342ac5125f4985aa37c40004012a25090165ad7bfb037e376fdf0df1d2caa793a1f94418544172f1b9e0ee19c068a0a070c'
